dd4ccc3a9808d46934a3f126634f06a810b3f86a
Jaroslav Týc <mail@jaroslavtyc.com>
2026-04-10 14:47:53 +0200

    SLS-59 Auto-load services of this plugin

--- src/ThreeBRSSyliusPacketaPlugin.php
+++ src/ThreeBRSSyliusPacketaPlugin.php
@@ -5,9 +5,18 @@ declare(strict_types=1);
 namespace ThreeBRS\SyliusPacketaPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ThreeBRSSyliusPacketaPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function build(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
+        $loader->load('services.yaml');
+    }
 }
